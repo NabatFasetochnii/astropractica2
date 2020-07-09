5d14982813f4f04a2de329c3bbabac19a594c082
Nikita <nikita.googlov@gmail.com>
2020-07-09 15:37:55 +0500

    1

--- MyForm.cpp
+++ MyForm.cpp
@@ -3,12 +3,23 @@
 
 using namespace System;
 
+//формулы перехода от точкин на поверхности сферы к точке на экране
 double cordX(double fi, double teta);
-double cordX(array<double>^ Array);
+double cordX(array<double>^ Array); // в массиве на вход первый элемент фи, второй - тета
 double cordY(double fi, double teta);
 double cordY(array<double>^ Array);
-array<double>^ cordXY(double fi, double teta);
+array<double>^ cordXY(double fi, double teta); // в массиве на выход первый элемент х, второй - у
 array<double>^ cordXY(array<double>^ Array);
+//////////////////////////////////////////////////////////////////////
+//координаты сферические по кооридинатам на экране (решения у этой штуки должно быть два) - слова аналитика
+double cordFI(double x, double y);
+double cordFI(array<double>^ Array); //аналогичная логика
+double cordTETA(double x, double y);
+double cordTETA(array<double>^ Array);
+array<double>^ cordFI_TETA(double x, double y);
+array<double>^ cordFI_TETA(array<double>^ Array);
+//////////////////////////////////////////////////////////////////////
+
 
 int main() {
 
@@ -68,6 +79,33 @@ array<double>^ cordXY(double fi, double teta) {
 
 array<double>^ cordXY(array<double>^ Array) {
     array<double>^ a = { cordX(Array[0], Array[1]), cordY(Array[0], Array[1]) };
+
+    return a;
+}
+
+double cordFI(double x, double y) {
+    return y/x;
+}
+
+double cordTETA(double x, double y) {
+    return atan(sqrt((pow(x, 2.0) + pow(y, 2)) / (sqrt(1 - pow(x, 2.0) - pow(y, 2)))));
+}
+
+double cordFI(array<double>^ Array) {
+    return cordFI(Array[0], Array[1]);
+}
+
+double cordTETA(array<double>^ Array) {
+    return cordTETA(Array[0], Array[1]);
+}
+
+array<double>^ cordFI_TETA(double x, double y) {
+    array<double>^ a = { cordFI(x, y), cordTETA(x, y) };
+    return a;
+}
+
+array<double>^ cordFI_TETA(array<double>^ Array) {
+    array<double>^ a = { cordFI(Array[0], Array[1]), cordTETA(Array[0], Array[1]) };
 
     return a;
 }
